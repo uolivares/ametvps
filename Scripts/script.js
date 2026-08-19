d6a213b27c226de4e91f97b56741777aa5e03c95
Ulises Olivares <uolivares@gmail.com>
2026-08-19 19:13:57 -0300

    Add files via upload

---
+++ Scripts/script.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+function justNumbers(e)
+{
+var keynum = window.event ? window.event.keyCode : e.which;
+if ((keynum == 8) || (keynum == 46))
+return true;
+ 
+return /\d/.test(String.fromCharCode(keynum));
+}
